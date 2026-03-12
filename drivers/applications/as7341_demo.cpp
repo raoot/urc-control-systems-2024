6f08f1037ad67fd122a0cbf760f2007b372cbc65
raoot <sakinabluer@gmail.com>
2026-03-12 14:24:00 -0700

    Digital Filter

--- drivers/applications/as7341_demo.cpp
+++ drivers/applications/as7341_demo.cpp
@@ -4,10 +4,36 @@
 #include <libhal/pointers.hpp>
 #include <libhal/units.hpp>
 
+#include <science/resource_list.hpp>
+
 #include "../hardware_map.hpp"
 #include "../include/as7341.hpp"
 
+constexpr int sample_rate = 20;
+
+// digital filter to conjugate values
+// scipy.signal.firwin2(20, [0, fs / 2], [1.0, 0], fs = fs)
+constexpr std::array<float, sample_rate> filter_const = {
+  9.66174445e-05, 1.55986763e-04, 3.33618583e-04, 7.15587299e-04,
+  1.46574468e-03, 2.93946352e-03, 6.05407192e-03, 1.38743094e-02,
+  4.26119415e-02, 4.02822938e-01, 4.02822938e-01, 4.26119415e-02,
+  1.38743094e-02, 6.05407192e-03, 2.93946352e-03, 1.46574468e-03, 
+  7.15587299e-04, 3.33618583e-04, 1.55986763e-04, 9.66174445e-05
+};
+
 namespace sjsu::drivers {
+
+ // arr = buffer
+ // begin = pointer to the start of arr 
+ float filter(std::array<float, sample_rate> arr, int begin) {
+    float sum = 0;
+    for (uint16_t i = 0; i < sample_rate; i++) {
+      int idx = (begin + i) % sample_rate;
+      sum += (arr[idx] * filter_const[i]);
+    }
+    return sum;
+  }
+
 void application()
 {
   using namespace std::chrono_literals;
@@ -26,6 +52,11 @@ void application()
   hal::print<64>(*terminal, "Sensor Created \n");
   hal::delay(*counter, 150ms);
 
+  auto adc = resources::adc_0();
+  std::array<float, sample_rate> data;  // store filter values
+  data.fill(0);  // fill with 0s
+  int begin = 0;
+
   hal::print<64>(*terminal, "Loop \n");
   while (true) {
     hal::delay(*counter, 500ms);
@@ -38,14 +69,21 @@ void application()
     hal::delay(*counter, 300ms);
     std::array<hal::u16, 6> F5F8 = as7341_sensor.readAllChannels();
 
+    data[begin] = adc->read();
     // CLEAR (ADC Count Range: 0 – 65535)
-    hal::print<64>(*terminal, "CLEAR (F1F5): %u\n", F1F4[4]);
-    hal::print<64>(*terminal, "CLEAR (F5F8): %u\n", F5F8[4]);
+    hal::print<64>(*terminal, "CLEAR (F1F5): %u\n", filter(F1F4[4], begin));
+    hal::print<64>(*terminal, "CLEAR (F5F8): %u\n", filter(F5F8[4]), begin);
 
     // 515nm & 555nm (ADC Count Range: 0 – 65535)
-    hal::print<64>(*terminal, "F4 515nm: %u\n", F1F4[3]);  // cyan
-    hal::print<64>(*terminal, "F5 555nm: %u\n", F5F8[0]);  // green
-    // apply a digital filter to conjugate values
+    hal::print<64>(*terminal, "F4 515nm: %u\n", filter(F1F4[3], begin));  // cyan
+    hal::print<64>(*terminal, "F5 555nm: %u\n", filter(F5F8[0], begin));  // green
+
+    hal::delay(*counter, 100ms);
+
+    begin++;
+    if (begin > sample_rate - 1) {
+      begin = 0;
+    }
   }
 }
 }  // namespace sjsu::drivers
